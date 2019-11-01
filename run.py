03897e0d1b3b1d185bd36c22f67a2aa8c631531d
mlko53 <mlko53@stanford.edu>
2019-11-01 12:00:23 -0700

    fix tabbing

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 import argparse
 import csv
+import pandas as pd
 from pathlib import Path
 from lightgbm.sklearn import LGBMRegressor
 
 def preprocess(csv_path):
-	raise NotImplementedError()
+    df = pd.read_csv(csv_path)
+
+    return df
+
 
 def main(args):
-	model_path = Path(args.model_path)
-	csv_path = Path(args.csv_path)
+    model_path = Path(args.model_path)
+    csv_path = Path(args.csv_path)
 
-	with model_path.open("rb") as f:
+    with model_path.open("rb") as f:
         model = pickle.load(f)
 
     user_input = preprocess(csv_path)
@@ -20,7 +24,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='RealNVP on CIFAR-10')
+    parser = argparse.ArgumentParser(description='ML for Risk Adjustment')
     parser.add_argument('--csv_path', required=True, type=str, help='Path to csv location',)
     parser.add_argument('--model_path', default='./checkpoints/sdh_model.pkl', type=str, help='Path to csv location')
     parser.add_argument('--save_path', default=None, type=str, help='Path to save location')
